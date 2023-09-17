e63397065474052c5fc660769bfc93275e7ab971
abhisheksarkar04 <abhisheksarkar0498@.com>
2023-09-18 02:08:43 +0530

    Navbar Code

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,15 @@
 import React, { useState } from 'react'
-import {Link} from "react-router-dom"
 import "./NavbarStyles.css"
 import {FaBars} from "react-icons/fa"
 import {ImCross} from "react-icons/im"
 
 
 const Navbar = () => {
-
-  const [Mobile,setMobile] = useState(true)
-
+  const [Mobile,setMobile] = useState(false)
 
   return (
+    <>
     <nav className = 'navbar'>
-
 
         <h3 className = 'logo'>Magetor</h3>
 
@@ -26,13 +23,14 @@ const Navbar = () => {
 
       </ul>
 
-        <button>Login/Sign Up</button>
+        <button className='LogSi'>Login/Sign Up</button>
 
         <button className = 'mobile-menu-icon'
             onClick   = {()=>setMobile(!Mobile)}>
             {Mobile? <ImCross/> : <FaBars />}
         </button>
     </nav>
+    </>
   )
 }
 
